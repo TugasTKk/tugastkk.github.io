afc7702ca43af09c238b9d7d5519f43be492d13b
tugasSBD <ian25yola@gmail.com>
2021-11-16 19:32:18 +0700

    baru

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -25,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 page = event.target.getAttribute("href").substr(1);
                 console.log(`Ini page ${page}`)
                 loadPage(page);
-                location.reload();
+                window.location.reload();
             });
           });
         }
